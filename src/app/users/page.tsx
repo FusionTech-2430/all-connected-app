71948f2f7e6fafc9a66a5f630ffec960a4528220
Moyano1711 <moyanoc-j@javeriana.edu.co>
2024-09-22 10:27:15 -0500

    :sparkles: Frontend user creation

---
+++ src/app/users/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Users from '@/components/Users'; 
+
+export default function UsersPage() {
+  return (
+    <div className="container mx-auto p-6">
+      <Users />
+    </div>
+  );
+}
