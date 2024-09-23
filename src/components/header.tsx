0845e7480386735bb41f9f6f550560b1a1a1cd9e
Camilo Nossa <calejandro_noss@outlook.com>
2024-09-22 21:28:26 -0500

    ✨Add public header component

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -22,10 +22,11 @@ import {
 } from '@radix-ui/react-dropdown-menu'
 import { Avatar, AvatarFallback } from '@radix-ui/react-avatar'
 import NavBar from './nav-bar'
+import Link from 'next/link'
 
 type HeaderProps = React.ComponentProps<'header'>
 
-export default function Header({ children, ...props }: HeaderProps) {
+export function AppHeader({ children, ...props }: HeaderProps) {
   return (
     <>
       <header {...props}>
@@ -91,5 +92,60 @@ export default function Header({ children, ...props }: HeaderProps) {
         </div>
       </header>
     </>
+  )
+}
+
+export function PublicHeader() {
+  return (
+    <header className="bg-primary-900 text-white p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="flex items-center space-x-2">
+          {/* <Globe className="h-6 w-6" /> */}
+          <Image
+            src={'/white-logo.png'}
+            alt="AllConnected logo"
+            width={32}
+            height={32}
+          />
+
+          <span className="font-bold text-xl">AllConnected</span>
+        </div>
+        <nav className="hidden md:block">
+          <ul className="flex space-x-4">
+            <li>
+              <Link href="#features" className="hover:underline">
+                Características
+              </Link>
+            </li>
+            <li>
+              <Link href="#pricing" className="hover:underline">
+                Precios
+              </Link>
+            </li>
+            <li>
+              <Link href="#benefits" className="hover:underline">
+                Acerca de
+              </Link>
+            </li>
+            <li>
+              <Link href="#contact-us" className="hover:underline">
+                Contacto
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <div className="hidden md:flex space-x-2">
+          <Button
+            variant="outline"
+            className="text-primary-900"
+          >
+            Iniciar Sesión
+          </Button>
+          <Button className="bg-blue-500 hover:bg-blue-600">
+            Crear Cuenta
+          </Button>
+        </div>
+      </div>
+    </header>
   )
 }
