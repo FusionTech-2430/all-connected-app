5292a42afc3a280b795c435e9434638e5b3226e8
Camilo Nossa <calejandro_noss@outlook.com>
2024-09-24 11:49:33 -0500

    🏷️ Add typescript support for firebase configuration

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,8 +1,8 @@
-import { initializeApp } from "firebase/app";
-import { getDatabase } from "firebase/database";
-import { getStorage } from "firebase/storage";
+import { initializeApp, type FirebaseOptions } from 'firebase/app'
+import { getDatabase } from 'firebase/database'
+import { getStorage } from 'firebase/storage'
 
-const firebaseConfig = {
+const firebaseConfig: FirebaseOptions = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
@@ -10,10 +10,10 @@ const firebaseConfig = {
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-};
+}
 
-const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
-const storage = getStorage(app);
+const app = initializeApp(firebaseConfig)
+const database = getDatabase(app)
+const storage = getStorage(app)
 
-export { database, storage };
+export { database, storage }
