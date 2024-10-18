0c10d9231587b9e8d02b9db6465e6f0bbba912a3
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2024-10-18 18:57:18 -0500

    ✨Admin sidebar integration

---
+++ src/components/ui-own/admin-nav-bar.tsx
@@ -0,0 +1,71 @@
+import React from 'react'
+import NavItem from './nav-item'
+
+import {
+  Calendar,
+  House,
+  LineChart,
+  Package,
+  ShoppingCart,
+  Users,
+  NotebookPen,
+  Wallet
+} from 'lucide-react'
+
+const navBarItems = [
+  {
+    uid: 'business',
+    href: '/admin/business',
+    title: 'Empredimientos',
+    icon: <Package size={20} />
+  },
+  {
+    uid: 'events',
+    href: '/admin/event',
+    title: 'Eventos',
+    icon: <Calendar size={20} />
+  },
+  {
+    uid: 'products',
+    href: '/admin/products',
+    title: 'Productos',
+    icon: <House size={20} />
+  },
+  {
+    uid: 'users',
+    href: '/admin/user',
+    title: 'Usuarios',
+    icon: <Users size={20} />
+  },
+  {
+    uid: 'organizations',
+    href: '/admin/organizations',
+    title: 'Organizaciones',
+    icon: <NotebookPen size={20} />
+  }
+]
+
+type NavBarProps = React.ComponentProps<'nav'>
+
+export default function NavBar({ children, ...props }: NavBarProps) {
+  return (
+    <>
+      <nav {...props}>
+        {/* Nav item */}
+
+        {navBarItems.map((item) => {
+          return (
+            <NavItem
+              key={item.uid}
+              href={item.href}
+              icon={item.icon}
+              title={item.title}
+            />
+          )
+        })}
+
+        {children}
+      </nav>
+    </>
+  )
+}
