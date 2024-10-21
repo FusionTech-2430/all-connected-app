ca9ad1c9a9d5da6eb631b73de5ba59979a979200
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-10-21 14:38:12 -0500

    :sparkles: add chat according to users

--- src/components/chat/MessagesList.tsx
+++ src/components/chat/MessagesList.tsx
@@ -2,20 +2,12 @@
 import { Send } from 'lucide-react'
 import Link from 'next/link'
 import { useState, useEffect } from 'react'
-import { ref, onValue } from 'firebase/database'
-import { database } from '@/lib/firebase/config'
 import { useRouter } from 'next/navigation'
-import { formatFriendlyDate } from '@/utils/date-utils'
-
-// Interfaz del mensaje
-interface Message {
-  id: string;
-  name: string;
-  date: string;
-}
+import { fetchUserChats } from '@/services/chatService'
+import { Chat } from '@/types/chat/Message'
 
 const MessagesList = () => {
-  const [messages, setMessages] = useState<Message[]>([]);
+  const [messages, setMessages] = useState<Chat[]>([]);
   const router = useRouter();
 
   useEffect(() => {
@@ -28,43 +20,8 @@ const MessagesList = () => {
             return;
           }
 
-          // Referencia a la colección 'chats'
-          const chatsRef = ref(database, 'chats');
-          
-          // Escuchar cambios en la colección 'chats'
-          onValue(chatsRef, (snapshot) => {
-            const chatsData = snapshot.val();
-            const userChats: Message[] = [];
-
-            // Filtrar los chats donde el usuario actual está presente
-            for (const chatId in chatsData) {
-              const chat = chatsData[chatId];
-              const usersInChat = chat.users;
-
-              // Verificar si el usuario actual está en este chat
-              if (Object.values(usersInChat).includes(id_user)) {
-                const otherUserId = Object.values(usersInChat).find((uid) => uid !== id_user);
-                const messages = chat.messages || {};
-                const messageKeys = Object.keys(messages);
-
-                let lastMessageDate = 'Sin mensajes';
-                // Si hay mensajes en el chat
-                if (messageKeys.length > 0) {
-                  const lastMessageKey = messageKeys[messageKeys.length - 1];
-                  const lastMessage = messages[lastMessageKey];
-                  lastMessageDate = formatFriendlyDate(lastMessage.time); // Aplicar formato amigable a la fecha
-                }
-
-                userChats.push({
-                  id: chatId,
-                  name: typeof otherUserId === 'string' ? otherUserId : 'Desconocido',
-                  date: lastMessageDate
-                });
-              }
-            }
-
-            setMessages(userChats);
-          });
+          // Obtener los chats del usuario
+          fetchUserChats(id_user, setMessages);
         }
       } catch (error) {
         console.error('Error getting chats:', error);
@@ -72,65 +29,48 @@ const MessagesList = () => {
     };
     fetchMessages();
   }, [router]);
+
   return (
-    <>
-      <section className="flex flex-col">
-        <div className="flex-1 p-6 bg-gray-100 dark:bg-gray-900 overflow-auto">
-          <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-6">
-            <h2 className="text-xl font-semibold mb-2 text-gray-900 dark:text-white">
-              Ver mensajes
-            </h2>
-            <p className="text-gray-600 dark:text-gray-400 mb-4">
-              Aquí se muestran todos los mensajes de tus clientes.
-            </p>
-            <div className="overflow-x-auto">
-              <table className="w-full">
-                <thead>
-                  <tr className="border-b border-gray-200 dark:border-gray-700">
-                    <th className="text-left py-2 text-gray-600 dark:text-gray-300">
-                      Cliente
-                    </th>
-                    <th className="text-left py-2 text-gray-600 dark:text-gray-300">
-                      Fecha
-                    </th>
-                    <th className="py-2">
-                      <span className="sr-only">Acciones</span>
-                    </th>
+    <section className="flex flex-col">
+      <div className="flex-1 p-6 bg-gray-100 dark:bg-gray-900 overflow-auto">
+        <div className="bg-white dark:bg-gray-800 rounded-lg shadow p-6">
+          <h2 className="text-xl font-semibold mb-2 text-gray-900 dark:text-white">
+            Ver mensajes
+          </h2>
+          <p className="text-gray-600 dark:text-gray-400 mb-4">
+            Aquí se muestran todos los mensajes de tus clientes.
+          </p>
+          <div className="overflow-x-auto">
+            <table className="w-full">
+              <thead>
+                <tr className="border-b border-gray-200 dark:border-gray-700">
+                  <th className="text-left py-2 text-gray-600 dark:text-gray-300">Cliente</th>
+                  <th className="text-left py-2 text-gray-600 dark:text-gray-300">Fecha</th>
+                  <th className="py-2">
+                    <span className="sr-only">Acciones</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {messages.map((message) => (
+                  <tr key={message.id} className="border-b border-gray-200 dark:border-gray-700">
+                    <td className="py-2 text-gray-900 dark:text-white">{message.name}</td>
+                    <td className="py-2 text-gray-900 dark:text-white">{message.date}</td>
+                    <td className="py-2 text-right">
+                      <Link href={`/chat?id=${message.id}`} className="inline-block text-blue-500 hover:text-blue-600 transition-colors">
+                        <Send className="h-5 w-5" />
+                        <span className="sr-only">Ver mensaje de {message.name}</span>
+                      </Link>
+                    </td>
                   </tr>
-                </thead>
-                <tbody>
-                  {messages.map((message) => (
-                    <tr
-                      key={message.id}
-                      className="border-b border-gray-200 dark:border-gray-700"
-                    >
-                      <td className="py-2 text-gray-900 dark:text-white">
-                        {message.name}
-                      </td>
-                      <td className="py-2 text-gray-900 dark:text-white">
-                        {message.date}
-                      </td>
-                      <td className="py-2 text-right">
-                        <Link
-                          href={`/chat?id=${message.id}`}
-                          className="inline-block text-blue-500 hover:text-blue-600 transition-colors"
-                        >
-                          <Send className="h-5 w-5" />
-                          <span className="sr-only">
-                            Ver mensaje de {message.name}
-                          </span>
-                        </Link>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
-      </section>
-    </>
+      </div>
+    </section>
   );
-}
+};
 
 export default MessagesList;
