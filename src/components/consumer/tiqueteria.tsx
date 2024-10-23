4e6bc6e6fc27db88f0182613a99c659ee62e27a8
Javier Alejandro Moyano Cipamocha <moyanoc-j@javeriana.edu.co>
2024-10-23 16:56:58 -0500

    :sparkles: Create tickets page

---
+++ src/components/consumer/tiqueteria.tsx
@@ -0,0 +1,21 @@
+import NavBar from '@/components/ui-own/nav-bar-consumer'
+
+const TicketsPage = () => {
+
+    return (
+      <div>
+        <NavBar />
+  
+        <section className="bg-[#0C4A6E] text-white py-20 mt-10">
+          <h1 className="text-4xl font-bold text-center">Tiquetera AllConnected</h1>
+        </section>
+  
+        <div className="flex mt-5">
+  
+  
+        </div>
+      </div>
+    )
+  }
+
+  export default TicketsPage
