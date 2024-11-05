5cfe6fbdf87fb8e3ff9527bdb4f9ecee9ae6956c
Camilo Nossa <calejandro_noss@outlook.com>
2024-11-04 20:05:01 -0500

    ✨Add error handling

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -10,16 +10,8 @@ import {
 
 export const signIn = async (email: string, password: string) => {
   const auth = getAuth()
-  try {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      email,
-      password
-    )
-    return userCredential
-  } catch (error) {
-    throw error
-  }
+  const userCredential = await signInWithEmailAndPassword(auth, email, password)
+  return userCredential
 }
 
 export const logOut = async () => {
